63cf2ff262af5493c9f76af465949fecffe6ccea
Guitong Geng <guitong.geng@gmail.com>
2018-03-06 19:48:43 +0800

    chore: update

--- dist/test.js
+++ dist/test.js
@@ -3,10 +3,36 @@
 var button = document.createElement('button');
 button.innerHTML = 'Click Me';
 
-Rx.Observable.fromEvent(button, 'click').throttleTime(1000).scan(function (count) {
-  return count + 1;
+Rx.Observable.fromEvent(button, 'click').throttleTime(1000).map(function (event) {
+  return event.clientX;
+}).scan(function (count, clientX) {
+  return count + clientX;
 }, 0).subscribe(function (count) {
-  return console.log('Clicked ' + count + ' times.');
+  return console.log('' + count);
 });
 
 document.body.appendChild(button);
+
+var observable = Rx.Observable.create(function (observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  setTimeout(function () {
+    observer.next(4);
+    observer.complete();
+  }, 1000);
+});
+
+console.log('just before subscribe');
+observable.subscribe({
+  next: function next(x) {
+    return console.log('got value ' + x);
+  },
+  error: function error(err) {
+    return console.error('error: ' + err);
+  },
+  complete: function complete() {
+    return console.log('done');
+  }
+});
+console.log('just after subscribe');
--- src/test.js
+++ src/test.js
@@ -3,7 +3,26 @@ button.innerHTML = 'Click Me';
 
 Rx.Observable.fromEvent(button, 'click')
   .throttleTime(1000)
-  .scan(count => count + 1, 0)
-  .subscribe(count => console.log(`Clicked ${count} times.`));
+  .map(event => event.clientX)
+  .scan((count, clientX) => count + clientX, 0)
+  .subscribe(count => console.log(`${count}`));
 
 document.body.appendChild(button);
+
+var observable = Rx.Observable.create(function (observer) {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  setTimeout(() => {
+    observer.next(4);
+    observer.complete();
+  }, 1000);
+});
+
+console.log('just before subscribe');
+observable.subscribe({
+  next: x => console.log('got value ' + x),
+  error: err => console.error('error: ' + err),
+  complete: () => console.log('done'),
+});
+console.log('just after subscribe');
